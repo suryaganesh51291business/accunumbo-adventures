a26c6aa432e082b18ce0700558fde22a4286fcd9
suryaganesh51291business <suryaganesh51291@gmail.com>
2026-08-26 21:28:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -151,9 +151,15 @@ app.get("/api/badge",(req,res)=>{
   });
 });
 
-app.get("*",(req,res)=>{
-  if(req.path.startsWith("/api/")) return res.status(404).json({error:"API route not found."});
-  res.sendFile(path.join(__dirname,"public","index.html"));
+pp.use((req, res) => {
+  if (req.path.startsWith("/api/")) {
+    return res.status(404).json({
+      error: "API route not found."
+    });
+  }
+
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 
 app.listen(PORT,()=>console.log(`AccuNumbo Adventures running on port ${PORT}`));
